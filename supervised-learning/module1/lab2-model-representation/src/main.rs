f0847c08388fe19be29521dcbf53f372ea7d1776
gausk <42976319+gausk@users.noreply.github.com>
2025-08-16 14:26:57 +0000

    model representation final

--- supervised-learning/module1/lab2-model-representation/src/main.rs
+++ supervised-learning/module1/lab2-model-representation/src/main.rs
@@ -1,6 +1,14 @@
 use ndarray::array;
 use plotters::prelude::*;
-use std::process::Command;
+
+fn compute_model_output(x: &ndarray::Array1<f64>, w: f64, b: f64) -> ndarray::Array1<f64> {
+    let m = x.len();
+    let mut f_wb = ndarray::Array1::zeros(m);
+    for i in 0..m {
+        f_wb[i] = w * x[i] + b;
+    }
+    f_wb
+}
 
 fn main() {
     let x_train = array![1.0, 2.0];
@@ -16,33 +24,23 @@ fn main() {
     let y_i = y_train[i];
     println!("(x^({i}), y^({i})) = ({x_i}, {y_i})");
 
-    /*
-        # Plot the data points
-    plt.scatter(x_train, y_train, marker='x', c='r')
-    # Set the title
-    plt.title("Housing Prices")
-    # Set the y-axis label
-    plt.ylabel('Price (in 1000s of dollars)')
-    # Set the x-axis label
-    plt.xlabel('Size (1000 sqft)')
-    plt.show()
-        */
-
     let root = BitMapBackend::new("model-representation.png", (500, 500)).into_drawing_area();
     root.fill(&WHITE).unwrap();
 
     let mut chart = ChartBuilder::on(&root)
         .caption("Housing Price", ("sans-serif", 30))
-        .margin(5)
+        .margin(10)
         .x_label_area_size(50)
-        .y_label_area_size(50)
-        .build_cartesian_2d(0f64..5f64, 0f64..600f64)
+        .y_label_area_size(70)
+        .build_cartesian_2d(0.8f64..2.2f64, 180f64..520f64)
         .unwrap();
 
     chart
         .configure_mesh()
         .x_desc("Size (1000 sqft)")
         .y_desc("Price (in 1000s of dollars)")
+        .label_style(("sans-serif", 15))
+        .y_label_offset(30)
         .draw()
         .unwrap();
 
@@ -51,12 +49,44 @@ fn main() {
             x_train
                 .iter()
                 .zip(y_train.iter())
-                .map(|(&x, &y)| Cross::new((x, y), 10, &RED)),
+                .map(|(&x, &y)| Cross::new((x, y), 10, RED)),
         )
+        .unwrap()
+        .label("Actual Values")
+        .legend(|(x, y)| Cross::new((x + 5, y), 5, RED));
+
+    let w = 100;
+    let b = 100;
+    println!("w: {w}");
+    println!("b: {b}");
+
+    let tmp_f_wb = compute_model_output(&x_train, w as f64, b as f64);
+    println!("tmp_f_wb = {tmp_f_wb}");
+
+    chart
+        .draw_series(LineSeries::new(
+            x_train.iter().zip(tmp_f_wb.iter()).map(|(&x, &y)| (x, y)),
+            BLUE,
+        ))
+        .unwrap()
+        .label("Our Prediction")
+        .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 10, y)], BLUE));
+
+    chart
+        .configure_series_labels()
+        .background_style(WHITE)
+        .border_style(BLACK)
+        .position(SeriesLabelPosition::UpperLeft)
+        .draw()
         .unwrap();
 
-    Command::new("catimg")
-        .arg("model-representation.png")
-        .status()
-        .expect("Failed to execute command");
+    root.present().unwrap();
+    println!("Plot saved to model-representation.png");
+
+    let w = 200.0;
+    let b = 100.0;
+    let x_i = 1.2;
+    let cost_1200sqft = w * x_i + b;
+
+    println!("${:.2} thousand dollars", cost_1200sqft);
 }
