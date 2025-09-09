7e4595a90769b3c39a2147e1de06341d3277228d
Gaurav Kumar <gauravsingh12341@gmail.com>
2025-09-09 18:44:54 +0530

    Week4 practice on decision tree complete

---
+++ advanced-learning-algorithms/module4/practice-w4-decision-tree/src/main.rs
@@ -0,0 +1,24 @@
+use lab1_decision_tree::{build_tree_recursive, feature_selection};
+
+fn main() {
+    println!("Welcome to practice lab of Week 4 on DecisionTree!");
+
+    let x_train = Vec::from([
+        vec![1, 1, 1],
+        vec![1, 0, 1],
+        vec![1, 0, 0],
+        vec![1, 0, 0],
+        vec![1, 1, 1],
+        vec![0, 1, 1],
+        vec![0, 0, 0],
+        vec![1, 0, 1],
+        vec![0, 1, 0],
+        vec![1, 0, 0],
+    ]);
+
+    let y_train = Vec::from([1, 1, 0, 0, 1, 0, 0, 1, 1, 0]);
+
+    feature_selection(&x_train, &y_train);
+
+    build_tree_recursive(&x_train, &y_train, 3);
+}
