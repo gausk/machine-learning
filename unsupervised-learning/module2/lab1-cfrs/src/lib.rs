c4811afe540e427ca8bde776cf8a35398015e455
Gaurav Kumar <gauravsingh12341@gmail.com>
2025-09-14 01:54:25 +0530

    Collaborative filtering complete

--- unsupervised-learning/module2/lab1-cfrs/src/lib.rs
+++ unsupervised-learning/module2/lab1-cfrs/src/lib.rs
@@ -1,7 +1,32 @@
 use csv::ReaderBuilder;
-use ndarray::Array2;
+use ndarray::Array;
+use ndarray::{Array2, Axis};
 use ndarray_csv::Array2Reader;
+use ndarray_rand::RandomExt;
+use ndarray_rand::rand_distr::StandardNormal;
+use serde::Deserialize;
+use serde::Serialize;
 use std::path::Path;
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct MovieInfo {
+    #[serde(rename = "mean rating")]
+    mean_rating: f64,
+    #[serde(rename = "number of ratings")]
+    number_of_ratings: u64,
+    pub title: String,
+}
+
+pub fn load_movie_info() -> Vec<MovieInfo> {
+    let path = Path::new(env!("CARGO_MANIFEST_DIR")).join("../data/small_movie_list.csv");
+    let mut rdr = ReaderBuilder::new().from_path(path).unwrap();
+    let mut movies = Vec::new();
+    for row in rdr.deserialize::<MovieInfo>() {
+        let record = row.unwrap();
+        movies.push(record);
+    }
+    movies
+}
 
 pub fn load_rating_small() -> (Array2<f64>, Array2<f64>) {
     let path = Path::new(env!("CARGO_MANIFEST_DIR")).join("../data");
@@ -47,10 +72,61 @@ pub fn collaborative_filtering_cost(
     b: &Array2<f64>,
     lambda: f64,
 ) -> f64 {
-    let mut pred = x.dot(&w.t());
-    pred += b;
+    let pred = x.dot(&w.t()) + b;
     let j = (pred - y) * r;
     let squared_error = 0.5 * j.mapv(|v| v.powi(2)).sum();
-    let reg = (lambda / 2.0) * (x.mapv(|v| v.powi(2)).sum() + w.map(|v| v.powi(2)).sum());
+    let reg = (lambda / 2.0) * (x.mapv(|v| v.powi(2)).sum() + w.mapv(|v| v.powi(2)).sum());
     squared_error + reg
+}
+
+pub fn normalize_rating(y: &Array2<f64>, r: &Array2<f64>) -> (Array2<f64>, Array2<f64>) {
+    let mean = (y * r).sum_axis(Axis(1)) / (r.sum_axis(Axis(1)) + 1e-12);
+    let mean2d = mean.insert_axis(Axis(1));
+    let y_mean = mean2d * r;
+    let ynorm = y - &y_mean;
+    (ynorm, y_mean)
+}
+
+pub fn collaborative_filtering_training(
+    y_norm: &Array2<f64>,
+    r: &Array2<f64>,
+    lambda: f64,
+    iterations: usize,
+    num_features: usize,
+    learning_rate: f64,
+) -> (Array2<f64>, Array2<f64>, Array2<f64>) {
+    let num_movies = y_norm.shape()[0];
+    let num_users = y_norm.shape()[1];
+    let mut x: Array2<f64> = Array::random((num_movies, num_features), StandardNormal);
+    let mut w: Array2<f64> = Array::random((num_users, num_features), StandardNormal);
+    let mut b: Array2<f64> = Array::random((1, num_users), StandardNormal);
+
+    for i in 0..iterations {
+        let cost = collaborative_filtering_cost(y_norm, r, &w, &x, &b, lambda);
+        let (d_x, d_w, d_b) = backward_pass(&x, &w, &b, y_norm, r, lambda);
+        x -= &(learning_rate * d_x);
+        w -= &(learning_rate * d_w);
+        b -= &(learning_rate * d_b);
+
+        if i % 50 == 0 {
+            println!("Cost at iteration {} is {}", i, cost);
+        }
+    }
+    (x, w, b)
+}
+
+pub fn backward_pass(
+    x: &Array2<f64>,
+    w: &Array2<f64>,
+    b: &Array2<f64>,
+    y: &Array2<f64>,
+    r: &Array2<f64>,
+    lambda: f64,
+) -> (Array2<f64>, Array2<f64>, Array2<f64>) {
+    let pred = x.dot(&w.t()) + b;
+    let e = (pred - y) * r;
+    let d_x = e.dot(w) + lambda * x;
+    let d_w = e.t().dot(x) + lambda * w;
+    let d_b = e.sum_axis(Axis(0)).insert_axis(Axis(0));
+    (d_x, d_w, d_b)
 }
--- unsupervised-learning/module2/lab1-cfrs/src/main.rs
+++ unsupervised-learning/module2/lab1-cfrs/src/main.rs
@@ -1,9 +1,12 @@
-use lab1_cfrs::{collaborative_filtering_cost, load_precalc_params_small, load_rating_small};
-use ndarray::s;
+use lab1_cfrs::{
+    collaborative_filtering_cost, collaborative_filtering_training, load_movie_info,
+    load_precalc_params_small, load_rating_small, normalize_rating,
+};
+use ndarray::{Array1, Axis, s};
 
 fn main() {
     println!("Welcome to Lab on Collaborative Filtering Recommender Systems");
-    let (y, r) = load_rating_small();
+    let (mut y, mut r) = load_rating_small();
     println!("Y shape: {:?}", y.shape());
     println!("R shape: {:?}", r.shape());
     let (x, w, b) = load_precalc_params_small();
@@ -49,4 +52,52 @@ fn main() {
         1.5,
     );
     println!("cost_with_reg: {}", cost_with_reg);
+    let movies = load_movie_info();
+    println!("First Movie: {:?}", movies[0]);
+
+    let mut my_ratings = Array1::<f64>::zeros(num_movies);
+    let mut my_r = Array1::<f64>::zeros(num_movies);
+    my_ratings[2700] = 5.0; // Toy Story 3 (2010)
+    my_ratings[2609] = 2.0; // Persuasion (2007)
+    my_ratings[929] = 5.0; // Lord of the Rings: The Return of the King, The
+    my_ratings[246] = 5.0; // Shrek (2001)
+    my_ratings[2716] = 3.0; // Inception
+    my_ratings[1150] = 5.0; // Incredibles, The (2004)
+    my_ratings[382] = 2.0; // Amelie (Fabuleux destin d'Amélie Poulain, Le)
+    my_ratings[366] = 5.0; // Harry Potter and the Sorcerer's Stone (a.k.a. Harry Potter and the Philosopher's Stone) (2001)
+    my_ratings[622] = 5.0; // Harry Potter and the Chamber of Secrets (2002)
+    my_ratings[988] = 3.0; // Eternal Sunshine of the Spotless Mind (2004)
+    my_ratings[2925] = 1.0; // Louis Theroux: Law & Disorder (2008)
+    my_ratings[2937] = 1.0; // # Nothing to Declare (Rien à déclarer)
+    my_ratings[793] = 5.0; // Pirates of the Caribbean: The Curse of the Black Pearl (2003)
+
+    let mut my_rated = vec![];
+    for i in 0..num_movies {
+        if my_ratings[i] > 0.0 {
+            my_rated.push(i);
+            my_r[i] = 1.0;
+            println!("My rating {} for movie {}", my_ratings[i], movies[i].title);
+        }
+    }
+    y.push(Axis(1), (&my_ratings).into()).unwrap();
+    r.push(Axis(1), (&my_r).into()).unwrap();
+    println!("y new shape: {:?}", y.shape());
+    println!("r new shae: {:?}", r.shape());
+    let (y_norm, y_mean) = normalize_rating(&y, &r);
+    let (x_o, w_o, b_o) = collaborative_filtering_training(&y_norm, &r, 1.0, 1000, 100, 0.001);
+    println!("x_o shape: {:?}", x_o.shape());
+    println!("w_o shape: {:?}", w_o.shape());
+    println!("b_o shape: {:?}", b_o.shape());
+
+    let predict = x_o.dot(&w_o.t()) + b_o + y_mean;
+    println!("predict shape {:?}", predict.shape());
+
+    for idx in my_rated {
+        println!(
+            "Predicted rating: {}, actual rating: {} for movie {}",
+            y[[idx, num_users]],
+            predict[[idx, num_users]],
+            movies[idx].title
+        );
+    }
 }
